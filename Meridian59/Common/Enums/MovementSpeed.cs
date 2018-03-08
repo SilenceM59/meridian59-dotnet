73a3eb7347ea12f835e83fac6c3da1e907ca2639
Nathan Huebner <ndallas1983@gmail.com>
2018-03-07 19:08:04 -0600

    Set the client run speed back to 50 - this will be adjusted by spells or something in the future.

--- Meridian59/Common/Enums/MovementSpeed.cs
+++ Meridian59/Common/Enums/MovementSpeed.cs
@@ -32,7 +32,7 @@ namespace Meridian59.Common.Enums
         SPEED_VERY_FAST     = 20,
 
         USER_WALKING_SPEED  = 25,
-        USER_RUNNING_SPEED  = 100,
+        USER_RUNNING_SPEED  = 50,
 
         Teleport    = SPEED_NONE,
         Walk        = USER_WALKING_SPEED,
