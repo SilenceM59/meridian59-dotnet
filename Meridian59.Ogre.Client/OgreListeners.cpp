4c213778b4f247d3ea05a53a05b7e63e43675776
Nathan Huebner <ndallas1983@gmail.com>
2018-03-17 18:23:58 -0500

    ALT+K+D formatting - Nothing in here was actually changed.

--- Meridian59.Ogre.Client/OgreListeners.cpp
+++ Meridian59.Ogre.Client/OgreListeners.cpp
@@ -1,227 +1,233 @@
 #include "stdafx.h"
 
-namespace Meridian59 { namespace Ogre 
-{
-   MyFrameListener::MyFrameListener()
-   {
-   };
+namespace Meridian59 {
 
-   bool MyFrameListener::frameRenderingQueued (const FrameEvent &evt)
-   {
-      return true;
-   };
+	namespace Ogre
+	{
+		MyFrameListener::MyFrameListener()
+		{
+		};
 
-   ////////////////////////////////////////////
+		bool MyFrameListener::frameRenderingQueued(const FrameEvent &evt)
+		{
+			return true;
+		};
 
-   MyWindowEventListener::MyWindowEventListener()
-   {
-   };
+		////////////////////////////////////////////
 
-   bool MyWindowEventListener::windowClosing (RenderWindow* rw)
-   {
-      // mark for shutdown ourself
-      OgreClient::Singleton->IsRunning = false;
+		MyWindowEventListener::MyWindowEventListener()
+		{
+		};
 
-      // ignore window closing here
-      return false;
-   };
+		bool MyWindowEventListener::windowClosing(RenderWindow* rw)
+		{
+			// mark for shutdown ourself
+			OgreClient::Singleton->IsRunning = false;
 
-   void MyWindowEventListener::windowFocusChange (RenderWindow* rw)
-   {
-      // fix bug in cegui with some windows not being drawn
-      // after ctrl+alt+entf or long idle
-      if (ControllerUI::IsInitialized && ControllerUI::GUIRoot)
-         ControllerUI::GUIRoot->invalidate(true);
-   };
+			// ignore window closing here
+			return false;
+		};
 
-   ////////////////////////////////////////////
+		void MyWindowEventListener::windowFocusChange(RenderWindow* rw)
+		{
+			// fix bug in cegui with some windows not being drawn
+			// after ctrl+alt+entf or long idle
+			if (ControllerUI::IsInitialized && ControllerUI::GUIRoot)
+				ControllerUI::GUIRoot->invalidate(true);
+		};
 
-   CameraListener::CameraListener()
-   {
-   };
+		////////////////////////////////////////////
 
-   CameraListener::~CameraListener(void)
-   {
-   };
+		CameraListener::CameraListener()
+		{
+		};
 
-   void CameraListener::objectMoved(MovableObject* obj)
-   {
-      Camera*           camera     = OgreClient::Singleton->Camera;
-      SceneNode*        cameraNode = OgreClient::Singleton->CameraNode;
-      RooFile^          room       = OgreClient::Singleton->CurrentRoom;
-      OgreClientConfig^ config     = OgreClient::Singleton->Config;
-      CaelumSystem*     caelum     = ControllerRoom::CaelumSystem;
+		CameraListener::~CameraListener(void)
+		{
+		};
 
-      if (!camera || !cameraNode || !room)
-         return;
+		void CameraListener::objectMoved(MovableObject* obj)
+		{
+			bool camera_set_to_zero = false;
+			
+			Camera*           camera = OgreClient::Singleton->Camera;
+			SceneNode*        cameraNode = OgreClient::Singleton->CameraNode;
+			RooFile^          room = OgreClient::Singleton->CurrentRoom;
+			OgreClientConfig^ config = OgreClient::Singleton->Config;
+			CaelumSystem*     caelum = ControllerRoom::CaelumSystem;
 
-      // get camera position in world
-      const ::Ogre::Vector3& posCam  = camera->getDerivedPosition();
-      const ::Ogre::Vector3& posNode = cameraNode->_getDerivedPosition();
+			if (!camera || !cameraNode || !room)
+				return;
 
-      // sanity check, would kill
-      if (posCam.isNaN() || posNode.isNaN())
-         return;
+			// get camera position in world
+			const ::Ogre::Vector3& posCam = camera->getDerivedPosition();
+			const ::Ogre::Vector3& posNode = cameraNode->_getDerivedPosition();
 
-      // notify caelum about change
-      if (caelum)
-         caelum->notifyCameraChanged(camera);
+			// sanity check, would kill
+			if (posCam.isNaN() || posNode.isNaN())
+				return;
 
-      // offset to stay away from possible room intersections
-      const CLRReal OFFSET = 8.0f;
+			// notify caelum about change
+			if (caelum)
+				caelum->notifyCameraChanged(camera);
 
-      // get start and end
-      V3% start = Util::ToV3(posNode);
-      V3% end   = Util::ToV3(posCam);
-      V3 newPos;
+			// offset to stay away from possible room intersections
+			const CLRReal OFFSET = 8.0f;
 
-      // create vector in same direction with length of offset
-      V3% dir = end + (-start);
-      dir.ScaleToLength(OFFSET);
+			// get start and end
+			V3% start = Util::ToV3(posNode);
+			V3% end = Util::ToV3(posCam);
+			V3 newPos;
 
-      // add this extension on end
-      end = end + dir;
+			// create vector in same direction with length of offset
+			V3% dir = end + (-start);
+			dir.ScaleToLength(OFFSET);
 
-      // convert to ROO format
-      start.ConvertToROO();
-      end.ConvertToROO();
+			// add this extension on end
+			end = end + dir;
 
-      // verify new camera location
-      if (config->CameraCollisions && !room->VerifySight(start, end, newPos))
-      {
-         // convert back to world (height in z)
-         newPos = newPos.XZY;
-         newPos.ConvertToWorld();
+			// convert to ROO format
+			start.ConvertToROO();
+			end.ConvertToROO();
 
-         ::Ogre::Vector3& oInter = Util::ToOgre(newPos);
-         ::Ogre::Vector3& oDelta = (oInter - posNode);
-         ::Ogre::Real len = oDelta.length();
-         
-         // move camera a bit away from actual intersection closer to avatar
-         len = (::Ogre::Real)CLRMath::Max((CLRReal)len - OFFSET, (CLRReal)0.0f);
+			// verify new camera location
+			if (config->CameraCollisions && !room->VerifySight(start, end, newPos))
+			{
+				// convert back to world (height in z)
+				newPos = newPos.XZY;
+				newPos.ConvertToWorld();
 
-         // set camera to intersection (internal space)
-         camera->setPosition(0.0f, 0.0f, len);
-      }
-      else
-         newPos = Util::ToV3(posCam);
+				::Ogre::Vector3& oInter = Util::ToOgre(newPos);
+				::Ogre::Vector3& oDelta = (oInter - posNode);
+				::Ogre::Real len = oDelta.length();
 
-      //::System::Console::WriteLine(newPos.X.ToString() + "  " + newPos.Y.ToString() + "  " + newPos.Z.ToString());
+				// move camera a bit away from actual intersection closer to avatar
+				len = (::Ogre::Real)CLRMath::Max((CLRReal)len - OFFSET, (CLRReal)0.0f);
 
-      // update viewer position in datalayer
-      OgreClient::Singleton->Data->ViewerPosition = newPos;
+				// set camera to intersection (internal space)
+				camera->setPosition(0.0f, 0.0f, len);
 
-      // quit if controllers not initialized
-      if (!ControllerUI::IsInitialized || !ControllerInput::IsInitialized)
-         return;
+				camera_set_to_zero = true;
+			}
+			else {
+				newPos = Util::ToV3(posCam);
+			}
 
-      // also quit if mouse on cegui control or button down
-      if (ControllerUI::TopControl != nullptr || ControllerInput::IsAnyMouseDown)
-         return;
+			// update viewer position in datalayer
+			OgreClient::Singleton->Data->ViewerPosition = newPos;
 
-      // get mouse coords
-      const OIS::MouseState& mouseState = ControllerInput::OISMouse->getMouseState();
+			// quit if controllers not initialized
+			if (!ControllerUI::IsInitialized || !ControllerInput::IsInitialized)
+				return;
 
-      // perform mouseover
-      ControllerInput::PerformMouseOver(mouseState.X.abs, mouseState.Y.abs, false);
-   };
+			// also quit if mouse on cegui control or button down
+			if (ControllerUI::TopControl != nullptr || ControllerInput::IsAnyMouseDown)
+				return;
 
-   ////////////////////////////////////////////
+			// get mouse coords
+			const OIS::MouseState& mouseState = ControllerInput::OISMouse->getMouseState();
 
-   void CompositorPainListener::notifyMaterialSetup( uint32 pass_id, MaterialPtr & mat )
-   {
-   };
+			// perform mouseover
+			ControllerInput::PerformMouseOver(mouseState.X.abs, mouseState.Y.abs, false);
+		};
 
-   void CompositorPainListener::notifyMaterialRender( uint32 pass_id, MaterialPtr & mat )
-   {
-      // build a red blendcolor with alpha based on progress
-      // we scale pain effect here from 30-0% blending
-      float alpha = 0.3f - (0.3f * (float)OgreClient::Singleton->Data->Effects->Pain->Progress);
-      float blendcolor[4];
-      blendcolor[0] = 1.0f;  // r
-      blendcolor[1] = 0.0f;  // g
-      blendcolor[2] = 0.0f;  // b
-      blendcolor[3] = alpha; // a
+		////////////////////////////////////////////
 
-      // get shader parameters
-      const GpuProgramParametersSharedPtr list = 
-         mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
+		void CompositorPainListener::notifyMaterialSetup(uint32 pass_id, MaterialPtr & mat)
+		{
+		};
 
-      // set current blendcolor in shader
-      list->setNamedConstant(SHADERBLENDCOLOR, blendcolor, 1);
-   };
+		void CompositorPainListener::notifyMaterialRender(uint32 pass_id, MaterialPtr & mat)
+		{
+			// build a red blendcolor with alpha based on progress
+			// we scale pain effect here from 30-0% blending
+			float alpha = 0.3f - (0.3f * (float)OgreClient::Singleton->Data->Effects->Pain->Progress);
+			float blendcolor[4];
+			blendcolor[0] = 1.0f;  // r
+			blendcolor[1] = 0.0f;  // g
+			blendcolor[2] = 0.0f;  // b
+			blendcolor[3] = alpha; // a
 
-   void CompositorPainListener::notifyResourcesCreated(bool forResizeOnly)
-   {
-   };
+			// get shader parameters
+			const GpuProgramParametersSharedPtr list =
+				mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
 
-   ////////////////////////////////////////////
+			// set current blendcolor in shader
+			list->setNamedConstant(SHADERBLENDCOLOR, blendcolor, 1);
+		};
 
-   void CompositorWhiteoutListener::notifyMaterialSetup( uint32 pass_id, MaterialPtr & mat )
-   {
-   };
+		void CompositorPainListener::notifyResourcesCreated(bool forResizeOnly)
+		{
+		};
 
-   void CompositorWhiteoutListener::notifyMaterialRender( uint32 pass_id, MaterialPtr & mat )
-   {
-      // build a white blendcolor with alpha based on progress
-      float alpha = 1.0f - (float)OgreClient::Singleton->Data->Effects->Whiteout->Progress;
-      float blendcolor[4];
-      blendcolor[0] = 1.0f;  // r
-      blendcolor[1] = 1.0f;  // g
-      blendcolor[2] = 1.0f;  // b
-      blendcolor[3] = alpha; // a
+		////////////////////////////////////////////
 
-      // get shader parameters
-      const GpuProgramParametersSharedPtr list = 
-         mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
+		void CompositorWhiteoutListener::notifyMaterialSetup(uint32 pass_id, MaterialPtr & mat)
+		{
+		};
 
-      // set current blendcolor in shader
-      list->setNamedConstant(SHADERBLENDCOLOR, blendcolor, 1);
-   };
+		void CompositorWhiteoutListener::notifyMaterialRender(uint32 pass_id, MaterialPtr & mat)
+		{
+			// build a white blendcolor with alpha based on progress
+			float alpha = 1.0f - (float)OgreClient::Singleton->Data->Effects->Whiteout->Progress;
+			float blendcolor[4];
+			blendcolor[0] = 1.0f;  // r
+			blendcolor[1] = 1.0f;  // g
+			blendcolor[2] = 1.0f;  // b
+			blendcolor[3] = alpha; // a
 
-   void CompositorWhiteoutListener::notifyResourcesCreated(bool forResizeOnly)
-   {
-   };
+			// get shader parameters
+			const GpuProgramParametersSharedPtr list =
+				mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
 
-   ////////////////////////////////////////////
+			// set current blendcolor in shader
+			list->setNamedConstant(SHADERBLENDCOLOR, blendcolor, 1);
+		};
 
-   void LoadingBarResourceGroupListener::resourceGroupScriptingStarted(const String &groupName, size_t scriptCount)
-   {
-      ControllerUI::LoadingBar::ResourceGroupScriptingStarted(&groupName, scriptCount);
-   };
+		void CompositorWhiteoutListener::notifyResourcesCreated(bool forResizeOnly)
+		{
+		};
 
-   void LoadingBarResourceGroupListener::scriptParseStarted(const String &scriptName, bool &skipThisScript)
-   {
-      ControllerUI::LoadingBar::ScriptParseStarted(&scriptName, skipThisScript);
-   };
+		////////////////////////////////////////////
 
-   void LoadingBarResourceGroupListener::scriptParseEnded(const String &scriptName, bool skipped)
-   {
-      ControllerUI::LoadingBar::ScriptParseEnded(&scriptName, skipped);
-   };
+		void LoadingBarResourceGroupListener::resourceGroupScriptingStarted(const String &groupName, size_t scriptCount)
+		{
+			ControllerUI::LoadingBar::ResourceGroupScriptingStarted(&groupName, scriptCount);
+		};
 
-   void LoadingBarResourceGroupListener::resourceGroupLoadStarted(const String &groupName, size_t resourceCount)
-   {
-      ControllerUI::LoadingBar::ResourceGroupLoadStarted(&groupName, resourceCount);
-   };
+		void LoadingBarResourceGroupListener::scriptParseStarted(const String &scriptName, bool &skipThisScript)
+		{
+			ControllerUI::LoadingBar::ScriptParseStarted(&scriptName, skipThisScript);
+		};
 
-   void LoadingBarResourceGroupListener::resourceGroupPrepareStarted(const String &groupName, size_t resourceCount)
-   {
-      ControllerUI::LoadingBar::ResourceGroupPrepareStarted(&groupName, resourceCount);
-   };
+		void LoadingBarResourceGroupListener::scriptParseEnded(const String &scriptName, bool skipped)
+		{
+			ControllerUI::LoadingBar::ScriptParseEnded(&scriptName, skipped);
+		};
 
-   void LoadingBarResourceGroupListener::resourceLoadStarted(const ResourcePtr &resource)
-   {
-      ControllerUI::LoadingBar::ResourceLoadStarted(resource);
-   };
+		void LoadingBarResourceGroupListener::resourceGroupLoadStarted(const String &groupName, size_t resourceCount)
+		{
+			ControllerUI::LoadingBar::ResourceGroupLoadStarted(&groupName, resourceCount);
+		};
 
-   void LoadingBarResourceGroupListener::worldGeometryStageStarted(const String &description)
-   {
-      ControllerUI::LoadingBar::WorldGeometryStageStarted(&description);
-   };
+		void LoadingBarResourceGroupListener::resourceGroupPrepareStarted(const String &groupName, size_t resourceCount)
+		{
+			ControllerUI::LoadingBar::ResourceGroupPrepareStarted(&groupName, resourceCount);
+		};
 
-   void LoadingBarResourceGroupListener::worldGeometryStageEnded()
-   {
-      ControllerUI::LoadingBar::WorldGeometryStageEnded();
-   };
-};};
+		void LoadingBarResourceGroupListener::resourceLoadStarted(const ResourcePtr &resource)
+		{
+			ControllerUI::LoadingBar::ResourceLoadStarted(resource);
+		};
+
+		void LoadingBarResourceGroupListener::worldGeometryStageStarted(const String &description)
+		{
+			ControllerUI::LoadingBar::WorldGeometryStageStarted(&description);
+		};
+
+		void LoadingBarResourceGroupListener::worldGeometryStageEnded()
+		{
+			ControllerUI::LoadingBar::WorldGeometryStageEnded();
+		};
+	};
+};
